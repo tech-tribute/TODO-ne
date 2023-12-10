7e9ae76f86595f0474e32a43ccaf72f82c52ab91
Alii <aliireza.dev@gmail.com>
2023-12-10 17:03:53 +0330

    Define List and Todo models

--- TODOne/todos/models.py
+++ TODOne/todos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    parent_list = models.ForeignKey(
+        to="lists.List", related_name="todos", on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    is_complete = models.BooleanField(default=False)
+
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = "-create_date"
+        indexes = [models.Index(fields=["-create"])]
